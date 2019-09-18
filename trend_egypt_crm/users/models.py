d88ba26dde49f5ad1a4a4d46fb015d3b6ae9fee1
Mohamad A <mohammed.abuelhagag@gmail.com>
2019-09-18 02:00:37 +0200

    model

--- trend_egypt_crm/users/models.py
+++ trend_egypt_crm/users/models.py
@@ -11,10 +11,22 @@ class User(AbstractUser):
     name = models.CharField(_("Name of User"), blank=True, max_length=255)
     first_name = models.CharField(_("First Name"), blank=True, max_length=255)
     last_name = models.CharField(_("Last Name"), blank=True, max_length=255)
-    company = models.CharField(_("Company"), blank=True, max_length=255)
+    company = models.CharField(_("Company"), blank=True, max_length=20)
     phone = models.CharField(_("Phone"), blank=True, max_length=255)
     email = models.CharField(_("Email"), blank=True, max_length=255)
-    balance = models.CharField(_("Balance"), blank=True, max_length=255)
+    balance = models.DecimalField(_("Balance"), blank=True, decimal_places=2, max_digits=5)
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
+
+    @property
+    def authorization(self):
+        return "Unchecked"
+
+    @property
+    def reseller(self):
+        return "Unchecked"
+
+    @property
+    def private(self):
+        return "Unchecked"
